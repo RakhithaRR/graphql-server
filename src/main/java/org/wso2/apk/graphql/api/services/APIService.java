3ae52be8ebd6bd3046d44d5d332b41f914f5b9a8
RakhithRR <rakhitharr@live.com>
2023-02-17 17:56:42 +0530

    Add automatic tenant and user looping

--- src/main/java/org/wso2/apk/graphql/api/services/APIService.java
+++ src/main/java/org/wso2/apk/graphql/api/services/APIService.java
@@ -3,10 +3,12 @@ package org.wso2.apk.graphql.api.services;
 import org.wso2.apk.graphql.api.datatypes.APIDataType;
 import org.wso2.apk.graphql.api.fetchers.dto.APIListDTO;
 import org.wso2.apk.graphql.api.mappings.APIDataTypeMapper;
+import org.wso2.apk.graphql.api.utils.CommonUtils;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.model.API;
 import org.wso2.carbon.apimgt.rest.api.common.RestApiCommonUtil;
 import org.wso2.carbon.apimgt.api.APIProvider;
+import org.wso2.carbon.user.api.Tenant;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,21 +16,26 @@ import java.util.List;
 
 public class APIService {
     public static APIListDTO getAPIs() {
-        try {
-            APIListDTO apiListDTO = new APIListDTO();
-            List<APIDataType> apiList = new ArrayList<>();
-            APIProvider apiProvider = RestApiCommonUtil.getProvider("admin");
-            List<API> apis = apiProvider.getAllAPIs();
-            for (API api : apis) {
-                API detailedAPI = apiProvider.getAPIbyUUID(api.getUuid(), "carbon.super");
-                apiList.add(APIDataTypeMapper.mapAPIToAPIDataType(detailedAPI));
+        List<Tenant> organizations = CommonUtils.loadTenants();
+        APIListDTO apiListDTO = new APIListDTO();
+        for (Tenant organization : organizations) {
+            String adminUsername = organization.getAdminName();
+            String organizationName = organization.getDomain();
+            try {
+                List<APIDataType> apiList = new ArrayList<>();
+                APIProvider apiProvider = RestApiCommonUtil.getProvider(adminUsername);
+                List<API> apis = apiProvider.getAllAPIs();
+                for (API api : apis) {
+                    API detailedAPI = apiProvider.getAPIbyUUID(api.getUuid(), organizationName);
+                    apiList.add(APIDataTypeMapper.mapAPIToAPIDataType(detailedAPI));
+                }
+                apiListDTO.setCount(apiList.size());
+                apiListDTO.setList(apiList);
+            } catch (APIManagementException e) {
+                return null;
             }
-            apiListDTO.setCount(apiList.size());
-            apiListDTO.setList(apiList);
-            return apiListDTO;
-        } catch (APIManagementException e) {
-            return null;
         }
+        return apiListDTO;
     }
 
     public static APIDataType getAPI(String id) {
